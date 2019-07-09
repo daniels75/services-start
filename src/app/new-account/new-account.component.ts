28eba2d97076a1cff45c66a04120a8fdfac61d6c
Daniels <daniel.sadowski75@gmail.com>
2019-07-10 00:20:23 +0200

    Step 1 - added simple service

--- src/app/new-account/new-account.component.ts
+++ src/app/new-account/new-account.component.ts
@@ -1,18 +1,30 @@
 import { Component, EventEmitter, Output } from '@angular/core';
+import {LoggingService} from '../logging.service';
 
 @Component({
   selector: 'app-new-account',
   templateUrl: './new-account.component.html',
-  styleUrls: ['./new-account.component.css']
+  styleUrls: ['./new-account.component.css'],
+  providers: [LoggingService]
 })
 export class NewAccountComponent {
   @Output() accountAdded = new EventEmitter<{name: string, status: string}>();
+  constructor(private logging: LoggingService) {
 
+  }
   onCreateAccount(accountName: string, accountStatus: string) {
     this.accountAdded.emit({
       name: accountName,
       status: accountStatus
     });
-    console.log('A server status changed, new status: ' + accountStatus);
+    // 1st
+    // console.log('A server status changed, new status: ' + accountStatus);
+
+    // 2nd approach
+    //const logging: LoggingService = new LoggingService();
+    // logging.loggingStatusChanged(accountStatus);
+
+    // 3rd approach
+    this.logging.loggingStatusChanged(accountStatus);
   }
 }
