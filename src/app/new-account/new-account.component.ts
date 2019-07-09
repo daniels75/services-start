209efa8568f65a57a66d2a6fb8ed4d8c25123aa4
Daniels <daniel.sadowski75@gmail.com>
2019-07-10 00:51:50 +0200

    step3 added a service for data

--- src/app/new-account/new-account.component.ts
+++ src/app/new-account/new-account.component.ts
@@ -1,22 +1,19 @@
 import { Component, EventEmitter, Output } from '@angular/core';
 import {LoggingService} from '../logging.service';
+import {AccountService} from '../account.service';
 
 @Component({
   selector: 'app-new-account',
   templateUrl: './new-account.component.html',
   styleUrls: ['./new-account.component.css'],
-  providers: [LoggingService]
+  providers: [LoggingService, AccountService]
 })
 export class NewAccountComponent {
   @Output() accountAdded = new EventEmitter<{name: string, status: string}>();
-  constructor(private logging: LoggingService) {
-
+  constructor(private logging: LoggingService, private account: AccountService) {
   }
   onCreateAccount(accountName: string, accountStatus: string) {
-    this.accountAdded.emit({
-      name: accountName,
-      status: accountStatus
-    });
+    this.account.addAccount(accountName, accountStatus);
     // 1st
     // console.log('A server status changed, new status: ' + accountStatus);
 
